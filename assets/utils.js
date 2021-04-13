f231e7685d12bf74352eab21651afe7536aad832
grig.63116 <grig63116@gmail.com>
2021-04-13 10:48:08 +0400

    Fixes

--- assets/utils.js
+++ assets/utils.js
@@ -1,20 +1,19 @@
 const utils = {
-
     strReplaceAll (str, search, replacement) {
-        str = this.toString(str)
-        return str.replace(new RegExp(search, 'g'), replacement)
+        str = this.toString(str);
+        return str.replace(new RegExp(search, 'g'), replacement);
     },
     toString (p) {
-        return this.isStr(p) || this.isNum(p) ? p.toString() : ''
+        return this.isStr(p) || this.isNum(p) ? p.toString() : '';
     },
     isStr (p) {
-        return p && p.constructor.prototype === String.prototype
+        return p && p.constructor.prototype === String.prototype;
     },
     isNum (p) {
-        return p && p.constructor.prototype === Number.prototype && !this.isNaN(p)
+        return p && p.constructor.prototype === Number.prototype && !this.isNaN(p);
     },
     isNaN (p) {
-        return Number.isNaN(p)
+        return Number.isNaN(p);
     }
 }
 
